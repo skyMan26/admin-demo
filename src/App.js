80aaaacb05af3741b5d753138659cf42f079fddd
Hariharan Ganesan <ghariharan94@gmail.com>
2019-04-20 21:23:20 +0530

    refs#develop admin-demo initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
 class App extends Component {
   render() {
+    const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+<Admin dataProvider={dataProvider}>
+<Resource name ="users" list={ListGuesser}/>
+<Resource name ="ufg" list={ListGuesser}/>
+<Resource name ="sd" list={ListGuesser}/>
+</Admin>
       </div>
     );
   }
