078b576048467e15c0304ef58e285f2e1371b925
Hariharan Ganesan <ghariharan94@gmail.com>
2019-04-21 10:52:49 +0530

    refs#ticket-settingup added settings and finance to admin side menu

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
 <Resource name ="users" list={ListGuesser}/>
 <Resource name ="tenants " list={ListGuesser}/>
 <Resource name ="owners" list={ListGuesser}/>
+<Resource name ="Settings" list={ListGuesser}/>
+<Resource name ="Finance" list={ListGuesser}/>
 </Admin>
       </div>
     );
